51ec465d2ff96900966abeec679b9b6c2c1b0a4e
Marcel <marcellinuscnnaudi@gmail.com>
2022-06-11 00:43:48 +0100

    This is a string reverse code in js

---
+++ main.js
@@ -0,0 +1,9 @@
+//reverse a string in js mdn
+function solution(str){
+   return str.split("").reverse().join("");
+  
+}
+solution('world')
+
+//arrow function example
+const solution = str => str.split('').reverse().join('');
